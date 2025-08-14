ca23b17ece8c4a0ad35f4dca4b9e07796ab1f4f5
Lopez Garcia <978249@cognizant.com>
2025-08-14 14:48:45 -0400

    final push

---
+++ src/test/java/steps/BestBuySteps.java
@@ -0,0 +1,28 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class BestBuySteps {
+    WebDriver driver;
+
+    @Given("I am on the BestBuy home page")
+    public void i_am_on_the_best_buy_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("I am on the BestBuy home page");
+    }
+
+    @When("When I close the add modal and search for “macbook pro”")
+    public void when_i_close_the_add_modal_and_search_for_macbook_pro() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("When I close the add modal and search for “macbook pro”");
+    }
+
+    @Then("one of the laptops listed should be {double} 8GB Memory and 256GB SSD")
+    public void one_of_the_laptops_listed_should_be_8gb_memory_and_256gb_ssd(Double double1) {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("one of the laptops listed should be {double}” 8GB Memory and 256GB SSD");
+    }
+}
---
+++ src/test/resources/features/BestBuy.feature
@@ -0,0 +1,6 @@
+Feature: Searching on BestBuy
+
+  Scenario: searching for a product
+    Given I am on the BestBuy home page
+    When When I close the add modal and search for “macbook pro”
+    Then one of the laptops listed should be 13.3 8GB Memory and 256GB SSD
