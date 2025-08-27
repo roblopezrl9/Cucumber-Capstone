cf31f62cd5f36504c7d8679e983aeb4248b7b713
Abule Othow <2433393@cognizant.com>
2025-08-27 19:34:25 -0300

    MCP pipeline uses TestRunner class

--- src/test/java/steps/BestBuySteps.java
+++ src/test/java/steps/BestBuySteps.java
@@ -10,22 +10,16 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
-
-import static org.junit.Assert.fail;
 
 
 public class BestBuySteps {
     WebDriver driver = WebDriverManager.getDriver();
-  
+
     @Given("I am on the BestBuy home page")
     public void i_am_on_the_best_buy_home_page() {
 
@@ -53,54 +47,67 @@ public class BestBuySteps {
 
         System.out.println("When I close the add modal and search for “macbook pro”");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // wait for the input element to be visible
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("store-display-name")));
         WebElement inputBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("autocomplete-search-bar")));
+        // send keys to the input element
         inputBar.sendKeys("macbook pro");
+        // find the search button and click it
         WebElement searchButton = driver.findElement(By.id("autocomplete-search-button"));
+        // click the search button
         searchButton.click();
 
-        // Wait for the search results to load
-//        try{
-//            Thread.sleep(15000);
-//        }catch (Exception e){
-//            System.out.println("Error while waiting for search results to load: " + e.getMessage());
-//        }
     }
 
-    @Then("one of the laptops listed should be {double} 8GB Memory and 256GB SSD")
-    public void one_of_the_laptops_listed_should_be_8gb_memory_and_256gb_ssd(Double double1) {
+    @Then("one of the laptops listed should be 14 24GB Memory and 512GB SSD")
+    public void one_of_the_laptops_listed_should_be_14_24gb_memory_and_512gb_ssd() {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("one of the laptops listed should be {double}” 8GB Memory and 256GB SSD");
+        System.out.println("one of the laptops listed should be 14 8GB Memory and 256GB SSD");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement macBook = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'sku-block'][contains(., '16') and contains(., '24GB Memory') and contains(., '512GB SSD') and .//button[contains(., 'Add to cart')]]")));
+        // wait for the laptop with the specified specs to be visible
+        WebElement macBook = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'sku-block'][contains(., '16') and contains(., '24GB Memory') and " +
+                "contains(., '512GB SSD') and .//button[contains(., 'Add to cart')]]")));
+        // assert that the laptop with the specified specs is found
         Assert.assertTrue(macBook.getText().contains("16"));
         Assert.assertTrue(macBook.getText().contains("24GB Memory"));
         Assert.assertTrue(macBook.getText().contains("512GB SSD"));
-
-//        System.out.println("Found a 14 inch mac with 24 GB Memory and 512GB SSD: " + macBook.findElement(By.className("product-title")).getText());
 
     }
 
     @Given("I perform the above search")
     public void i_perform_the_above_search() {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("We are in the given i perform above");
+        // Get the title of the search results page and verify it contains "macbook pro"
+        String searchResult = driver.findElement(By.cssSelector("#promo-title")).getText();
+        Assert.assertTrue(searchResult.contains("macbook pro"), "Search result does not contain 'macbook pro'");
+
 
     }
     @When("I click the “Add to Cart” button next to the laptop")
     public void i_click_the_add_to_cart_button_next_to_the_laptop() {
         // Write code here that turns the phrase above into concrete actions
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement macBook = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'sku-block'][contains(., '16') and contains(., '24GB Memory') and contains(., '512GB SSD') and .//button[contains(., 'Add to cart')]]")));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement macBook = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'sku-block'][contains(., '16') and contains(., '24GB Memory') and " +
+                "contains(., '512GB SSD') and .//button[contains(., 'Add to cart')]]")));
         System.out.println(macBook.getText());
         WebElement button = wait.until(ExpectedConditions.elementToBeClickable(macBook.findElement(By.xpath(".//button[contains(., 'Add to cart')]"))));
-        button.click();
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", button );
+
+        try{
+            button.click();
+
+        }
+        catch (StaleElementReferenceException e){
+            System.out.println("Stale element reference exception caught");
+            js.executeScript("arguments[0].click();", button);
+        }
     }
     @Then("I should see a modal window with the cart subtotal")
     public void i_should_see_a_modal_window_with_the_cart_subtotal() {
-//         Write code here that turns the phrase above into concrete actions
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         System.out.println("I should see a modal window with the cart subtotal");
+        // wait for the cart subtotal to be visible
         WebElement cartSubtotal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-subtotal")));
         String actualText = cartSubtotal.getText();
         String expectedText = "Cart Subtotal";
@@ -112,6 +119,8 @@ public class BestBuySteps {
     public void i_am_on_the_best_buy_modal_page() {
         // Write code here that turns the phrase above into concrete actions
         System.out.println("I am in the Best Buy Modal page");
+        String modalText = driver.findElement(By.className("added-to-cart")).getText();
+        Assert.assertEquals(modalText, "Added to cart", "Modal text does not match expected text");
     }
 
     @When("I click on go to cart")
@@ -141,12 +150,10 @@ public class BestBuySteps {
     // THE REMOVE ITEM FROM CART
     @Given("I am on the Best Buy Cart page")
     public void i_am_on_the_best_buy_cart_page() {
-//        try{
-////            driver.get("https://www.bestbuy.com/cart");
-//            Thread.sleep(5000);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        // Write code here that turns the phrase above into concrete actions
+        String expUrl = "https://www.bestbuy.com/cart";
+        String actUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actUrl, expUrl);
     }
 
     @When("I remove an item from the cart")
@@ -162,12 +169,10 @@ public class BestBuySteps {
                     ExpectedConditions.elementToBeClickable(By.cssSelector("span.cart-label"))
             );
             cart.click();
-//            Thread.sleep(5000);
             WebElement removeButton = wait.until(
                     ExpectedConditions.elementToBeClickable(By.cssSelector("button.cart-item__remove"))
             );
             removeButton.click();
-//            Thread.sleep(5000);
         } catch (Exception e) {
             throw new RuntimeException("Cart is not clickable: " + e.getMessage());
         }
@@ -179,18 +184,19 @@ public class BestBuySteps {
     public void i_verify_that_the_item_is_removed_from_the_cart() {
         // Write code here that turns the phrase above into concrete actions
 
-        // h1 class="heading-5 page-heading__title" what shoudl show
-        // Your cart is empty
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        WebElement emptyCartMessage = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1.heading-5.page-heading__title"))
-        );
-        System.out.println("Message: "+ emptyCartMessage.getText());
-        String expectedMessage = "Your cart is empty";
-        String actualMessage = emptyCartMessage.getText();
-        org.junit.Assert.assertEquals(expectedMessage, actualMessage);
+        Boolean emptyCartMessage = wait.until(ExpectedConditions.textToBe(
+                By.cssSelector("h1.heading-5.page-heading__title"),
+                "Your cart is empty"
+        ));
+
+        Assert.assertTrue(emptyCartMessage, "The message does not match. The cart is not empty.");
+    }
+
+    @Then("I close the browser")
+    public void i_close_the_browser() {
+        // Write code here that turns the phrase above into concrete actions
         driver.quit();
     }
 }
-
