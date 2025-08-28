24c9383e373c9fa0512fd30b9e00122d1ee3b214
Abule Othow <2433393@cognizant.com>
2025-08-27 21:46:42 -0300

    MCP pipeline fix file name

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,16 +9,14 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "steps",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
-  
-        //tags = "@P21" // Uncomment to run specific tags
-//        tags = "@L1" //Login Functionality
-//         tags = "@L1" //Login Functionality
-
-        //tags = "@P22" //Product Search Function
-//        tags="not @L1 and not @L3" //Login with Data-Driven
-       tags="@T1"
-
+        plugin = {
+                "pretty", 
+                "html:target/cucumber-reports.html",
+                "json:mcp-results/cucumber.json",
+                "junit:mcp-results/junit.xml"
+        },
+        tags = "@T1",
+        monochrome = true
 )
 public class TestRunner {
 
